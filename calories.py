cba600c2fd436b463649cfc4010dea83afe0affc
AndreasEfth <44697219+AndreasEfth@users.noreply.github.com>
2018-11-20 16:30:45 +0100

    Update calories.py

--- calories.py
+++ calories.py
@@ -9,16 +9,16 @@ def define_met(m_user):
     """Function used to assign the MET for each activity preformed for the user """
     if m_user == '1':
         met = 3.5
-        print("You choose Squats as your activity")
+        #print("You choose Squats as your activity")
     elif m_user == '2':
         met = 8
-        print("You choose Walking on Stairs as your activity")
+        #print("You choose Walking on Stairs as your activity")
     elif m_user == '3':
         met = 8.8
-        print("You choose Jumps as your activity")
+        #print("You choose Jumps as your activity")
     else:
         met = 0
-        print("Wrong activity")
+        #print("Wrong activity")
     return met
 
 def calories_calculation(met_u, weight, time):
